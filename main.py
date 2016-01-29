833ed41fdf982f6489a6e9d10c5bd9d6522fc273
Dhruv Thakur <dhruvt93@gmail.com>
2016-01-29 15:53:40 +0530

    Minor changes to buttons on register, login

--- main.py
+++ main.py
@@ -491,4 +491,4 @@ def admin_remove_user(user_id):
 if __name__=="__main__":
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
-    app.run(debug=True)
+    app.run(debug=True,threaded=True)
